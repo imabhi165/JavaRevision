671f4180740154320ae977bd2db505b4c6d4ca0e
imabhi165 <abhi7808060981@gmail.com>
2026-02-08 22:28:36 +0530

    Added definition of object and classes in simple term.

--- src/OOps/Oops.java
+++ src/OOps/Oops.java
@@ -12,7 +12,13 @@ public class Oops {
         // What is an Object?
         // Object is a real entity created from a class What is an Object?
         // Object is a real entity created from a class 
-
+       //Classes :  classes are the blueprint.
+       //Example : If car is a class : then  bmw,martuti suzuki,etc are the object of that class.
+       //There are 4 pillars of the OOPS concepts :
+       //1.Encapsulation : -> 
+       //2.Abstraction
+       //3.Inheritence : Inheritance is the process where one class (child/subclass) inherits the properties and behaviors (fields and methods) of another class (parent/superclass).
+       //4.polymorphism : polymorphism means "Many forms" 
 
     }
 }
