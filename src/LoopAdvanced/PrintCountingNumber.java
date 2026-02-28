5fbd2de8a5a4e8b9d8a6d6907f024296d5ff933a
imabhi165 <abhi7808060981@gmail.com>
2026-02-28 22:10:19 +0530

    jc

--- src/LoopAdvanced/PrintCountingNumber.java
+++ src/LoopAdvanced/PrintCountingNumber.java
@@ -1,11 +1,13 @@
-package LoopAdvanced;
 import java.util.Scanner;
+
 public class PrintCountingNumber {
-    public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
-         int i;
-         for(i=1;i<=100;i++){
-             System.out.println(i);
-         }
+  public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+      System.out.println("Enter size of num to be print: ");
+      int size = sc.nextInt();
+      for (int i = 1; i <= size; i++) {
+        System.out.println(i);
+      }
     }
+  }
 }
