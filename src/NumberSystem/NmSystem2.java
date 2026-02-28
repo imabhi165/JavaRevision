e0fd3976448b92cdde6af6252e4ca0bec15856ed
imabhi165 <abhi7808060981@gmail.com>
2026-02-28 15:51:25 +0530

    jc

--- src/NumberSystem/NmSystem2.java
+++ src/NumberSystem/NmSystem2.java
@@ -1,19 +1,19 @@
-package NumberSystem;
 import java.util.Scanner;
-public class NmSystem2 {
-    public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the Value Here : ");
-        int decimal_num = sc.nextInt();
-        int ans = 0; //Binary number
-        int pw = 1 ; //it is representing power of 10
-        while(decimal_num > 0) {
-            int parity = decimal_num % 2;
-            ans +=(parity * pw);
-            pw*=10;
-            decimal_num /=2;
-        }
-        System.out.println("The conversion of binary to decimal is : "+ans);
 
+public class NmSystem2 {
+  public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+      System.out.print("Enter the Value Here : ");
+      int decimal_num = sc.nextInt();
+      int ans = 0; // Binary number
+      int pw = 1; // it is representing power of 10
+      while (decimal_num > 0) {
+        int parity = decimal_num % 2;
+        ans += (parity * pw);
+        pw *= 10;
+        decimal_num /= 2;
+      }
+      System.out.println("The conversion of binary to decimal is : " + ans);
     }
+  }
 }
